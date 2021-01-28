ac6902ab895259e0cd8cf5d47674378aca3500e5
jamie-ralph <jamesryanralph@outlook.com>
2021-01-28 16:10:22 +0000

    updated plastic bottles code

--- code/plastic2.R
+++ code/plastic2.R
@@ -98,13 +98,13 @@ plot_bottle_plot <- function(df, country_name, top_fill_radius) {
         geom_shape(data = bottle, aes(x = x, y = y), fill = NA, col = "#8d99ae",
                    expand = unit(-2, 'mm'), radius = unit(17.5, 'mm'), size = 1.5) +
         geom_shape(data = top, aes(x = x, y = y), fill = NA, col = "#8d99ae",
-                   radius = unit(5, 'mm'), size = 1.5) +
+                   radius = unit(3, 'mm'), size = 1.5) +
         geom_shape(data = country_tab, aes(x = x, y = y), fill = "#00b4d8",
                    expand = unit(-2, 'mm'), radius = unit(17.5, 'mm')) +
         geom_shape(data = country_tab_extra, aes(x = x, y = y), fill = "#00b4d8",
                    expand = unit(-2, 'mm'), radius = unit(top_fill_radius, 'mm')) +
         annotate("text", x = 1.25, y = 1.3, label = paste0(round(percent_value, 2) * 100, "%"), size = 8, col = "black", family = "Monda") +
-        xlim(c(0.1, 2.4)) +
+        xlim(c(0.4, 2.1)) +
         ggtitle(country_name) +
         theme(
             text = element_text(family = "Monda", colour = "#8d99ae"),
@@ -124,14 +124,16 @@ plot_bottle_plot <- function(df, country_name, top_fill_radius) {
 
 france <- plot_bottle_plot(proportions_unknown_by_country, "France", 10)
 germany <- plot_bottle_plot(proportions_unknown_by_country, "Germany", 16)
-uk <- plot_bottle_plot(proportions_unknown_by_country, "United Kingdom", 10)
-ireland <- plot_bottle_plot(proportions_unknown_by_country, "Ireland", 7)
-greece <- plot_bottle_plot(proportions_unknown_by_country, "Greece", 10)
-italy <- plot_bottle_plot(proportions_unknown_by_country, "Italy", 10)
+uk <- plot_bottle_plot(proportions_unknown_by_country, "United Kingdom", 6)
+ireland <- plot_bottle_plot(proportions_unknown_by_country, "Ireland", 1)
+greece <- plot_bottle_plot(proportions_unknown_by_country, "Greece", 1)
+italy <- plot_bottle_plot(proportions_unknown_by_country, "Italy", 1)
+denmark <- plot_bottle_plot(proportions_unknown_by_country, "Denmark", 1)
+netherlands <- plot_bottle_plot(proportions_unknown_by_country, "Netherlands", 5)
 
-plots_grid <- (germany + france + uk) /
-    (italy + ireland + greece) 
-    
+plots_grid <- (germany + france + uk + netherlands + plot_layout(nrow = 1)) /
+    (denmark +italy + ireland + greece + plot_layout(nrow = 1)) 
+
 
 plots_grid +
     plot_annotation(
@@ -140,14 +142,14 @@ plots_grid +
 Their annual Brand Audit uses plastic waste collected by volunteers<br> to identify the
 most polluting brands. A large portion of collected plastic has no identifiable brand. 
 This graphic explores the <span style = 'color:#00b4d8;'>proportion of unbranded plastic<br> waste</span> in a few select countries in 2020.\n\n",
-        caption = "Source: Break Free From Plastic | Data vis by @statsjam",
+        caption = "Source: Break Free From Plastic | Data vis by @statsjam for #TidyTuesday",
         theme = theme(
             plot.title = element_text(family = "Monda", colour = "#ef233c", size = 50, face = "bold"),
             plot.subtitle = element_textbox_simple(family = "Monda", colour = "#ef233c", size = 18,
                                                    padding = unit(c(0.5, 0, 1, 0), "cm")),
-            plot.caption = element_text(family = "Monda", colour = "#ef233c", size = 18, face = "bold"),
+            plot.caption = element_text(family = "Monda", colour = "#ef233c", size = 16, face = "bold"),
             plot.background = element_rect(fill = "#ffc8dd", color = NA)
         )
     ) 
 
-ggsave(here("images", "plastic_bottles.png"), width = 1920/72, height = 1080/72)
+#ggsave(here("images", "plastic_bottles.png"), width = 1920/72, height = 1080/72)
